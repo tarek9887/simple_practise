317c22d072cec43059aa023f1b9489e07b18a585
tarek9887 <139941002+tarek9887@users.noreply.github.com>
2023-10-18 19:32:14 +0600

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            string firstString = Console.ReadLine();
+            string secondString = Console.ReadLine();
+            for (int i = 0; i < firstString.Length; i++)
+            {
+                if (secondString[0] == firstString[i])
+                {
+                    count = i + 1;
+                    break;
+
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("Does not contain the alphabet");
+            }
+            else
+            {
+                Console.WriteLine(count);
+            }
+            Console.ReadLine();
+        }
+    }
+}
